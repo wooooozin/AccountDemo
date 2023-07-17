06da52861a6b57cd44ffd952ea333aa38fffd96a
wooooozin <wooooozin@gmail.com>
2023-07-17 23:26:06 +0900

    successDeleteAccount Test

--- src/test/java/com/example/accountdemo/controller/AccountControllerTest.java
+++ src/test/java/com/example/accountdemo/controller/AccountControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.accountdemo.controller;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.example.accountdemo.domain.Account;
 import com.example.accountdemo.dto.AccountDto;
 import com.example.accountdemo.dto.CreateAccount;
+import com.example.accountdemo.dto.DeleteAccount;
 import com.example.accountdemo.type.AccountStatus;
 import com.example.accountdemo.service.AccountService;
 import com.example.accountdemo.service.RedisTestService;
@@ -21,7 +22,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 @WebMvcTest(AccountController.class)
@@ -82,4 +82,29 @@ class AccountControllerTest {
                 .andExpect(status().isOk());
     }
 
+
+    @Test
+    void successDeleteAccount() throws Exception {
+        //given
+        given(accountService.deleteAccount(anyLong(), anyString()))
+                .willReturn(AccountDto.builder()
+                        .userId(1L)
+                        .accountNumber("1234567890")
+                        .registerAt(LocalDateTime.now())
+                        .unRegisteredAt(LocalDateTime.now())
+                        .build()
+                );
+        // when
+
+        // then
+        mockMvc.perform(delete("/account")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new DeleteAccount.Request(3333L, "1234567899")
+                        )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andDo(print());
+    }
 }
