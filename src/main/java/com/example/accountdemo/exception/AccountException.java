02455e2292e3b64fecf3064e1bfd93be814fc5e3
wooooozin <wooooozin@gmail.com>
2023-07-29 21:49:38 +0900

    AccountException final 상수로 변경

--- src/main/java/com/example/accountdemo/exception/AccountException.java
+++ src/main/java/com/example/accountdemo/exception/AccountException.java
@@ -4,14 +4,12 @@ import com.example.accountdemo.type.ErrorCode;
 import lombok.*;
 
 @Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 
 public class AccountException extends RuntimeException {
-    private ErrorCode errorCode;
-    private String errorMessage;
+    private final ErrorCode errorCode;
+    private final String errorMessage;
 
     public AccountException(ErrorCode errorCode) {
         this.errorCode = errorCode;
